6c46dca6dcb83836de9ff9f560e34d60f4b94bf3
markok0stic <94057185+markok0stic@users.noreply.github.com>
2022-11-26 18:18:39 +0100

    Board init

---
+++ src/board.py
@@ -0,0 +1,14 @@
+from const import *
+
+class Board:
+
+    def __init__(self):
+        self.h_rects = []
+        self.v_rects = []
+        pass
+
+    def _create(self):
+        pass
+
+    def _add_piesces(self, color):
+        pass
