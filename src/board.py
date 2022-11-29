97b476c3241a7582bfa9e85fe6d1000b331f0805
markok0stic <94057185+markok0stic@users.noreply.github.com>
2022-11-29 20:22:50 +0100

    Board hotfix

--- src/board.py
+++ src/board.py
@@ -1,17 +1,23 @@
 from square import Square
 from const import *
+
+
 class Board:
 
     def __init__(self):
         self.squares = []
 
     def _create(self):
+        print(Const.ROWS)
+        print(Const.COLS)
         row = []
-        for i in range(0, Const.ROWS):
+        for i in range(0, Const.COLS):
             row.append(0)
-        self.squares = [row for col in range(0, Const.COLS)]
+        self.squares = [row for col in range(Const.ROWS)]
 
         print(self.squares)
-        for i in range(0,Const.ROWS):
-            for j in range(0,Const.COLS):
-                self.squares[i][j] = Square(i,j)
+        for i in range(0, Const.ROWS - 2):
+            print(self.squares[i])
+            for j in range(0, Const.COLS - 2):
+                self.squares[i][j] = Square(i, j)
+
